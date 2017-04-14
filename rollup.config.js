b1a6cfa0294cb9447e7204d4da340ad178f09628
Gilles Coomans <gilles.coomans@gmail.com>
2017-04-14 23:36:21 +0200

    fix(build): finalise rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,27 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
-let pkg = require('./package.json');
-let external = Object.keys(pkg.dependencies);
+const pkg = require('./package.json');
+// const external = ['babelute', 'babelute-html-lexicon']; // Object.keys(pkg.dependencies);
 
 export default {
-  entry: 'src/index.js',
-  plugins: [ babel(babelrc()) ],
-  external: external,
-  targets: [
-    {
-      dest: pkg.main,
-      format: 'umd',
-      moduleName: '__MY__PROJECT__',
-      sourceMap: true
-    },
-    {
-      dest: pkg.module,
-      format: 'es',
-      sourceMap: true
-    }
-  ]
+	entry: 'src/index.js',
+	plugins: [
+		babel(babelrc()),
+		nodeResolve(),
+		commonjs()
+	],
+	external: ['babelute', 'babelute-html-lexicon'],
+	targets: [{
+		dest: pkg.main,
+		format: 'cjs',
+		sourceMap: true
+	}, {
+		dest: pkg.module,
+		format: 'es',
+		sourceMap: true
+	}]
 };
+
